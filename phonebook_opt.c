dabebb1c399852ab024043ea4fafcfb160a9a7c3
da255270 <da255270@hotmail.com>
2015-10-16 01:37:38 +0800

    correct some bug and modify hash function
    correct index type and move the printf function from findName to after return modify hash function to hash1 and hash2

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -34,12 +34,12 @@ hashTable *createHashTable(int tableSize)
 lastNameEntry* findName(char *key, hashTable *ht)
 {
     lastNameEntry *lne;
-    hashIndex index = hash(key, ht);
+    int index = hash1(key,ht);
+    //int index = hash2(key);
     
     /* search the bucket */
     for(lne = ht->list[index]; lne != NULL; lne = lne->pNext){
         if ( strcasecmp(key, lne->lastName) == 0 ){
-            printf(" %12s is found!\n", key);
             return lne;
         }
     }
@@ -49,7 +49,8 @@ lastNameEntry* findName(char *key, hashTable *ht)
 
 int append(char *key, hashTable *ht)
 {
-    hashIndex index = hash(key, ht);
+    int index = hash1(key,ht);
+    //int index = hash2(key); 
     lastNameEntry *newEntry;
     
     /* attempt to allocate memory for lastNameEntry */
@@ -64,8 +65,17 @@ int append(char *key, hashTable *ht)
 
     return 0;
 }
+/*djb2 + hash large prime : 42737*/
+int hash1(char *key, hashTable *table)
+{
+    unsigned long hashVal = 5381;   
+    int c;
+    while (c = *key++)
+       hashVal = ((hashVal << 5) + hashVal ) + c; /* hashVal * 33 + c */
+    return hashVal % table->tableSize;
+}
 /*djb2 + hash powers of 2 : 65536*/
-int hash(char *key)
+int hash2(char *key)
 {
     unsigned long hashVal = 5381;   
     int c;
